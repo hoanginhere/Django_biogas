22248cbeb8d8bf223db2bf9d76246fa1c4e39596
hoanginhere <130558884+hoanginhere@users.noreply.github.com>
2024-10-06 05:19:34 +0700

    Update asgi.py

--- biogas_monitoring/biogas_monitoring/asgi.py
+++ biogas_monitoring/biogas_monitoring/asgi.py
@@ -17,7 +17,7 @@ from django.core.asgi import get_asgi_application
 import datamanagement.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biogas_monitoring.settings')
-
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 application = ProtocolTypeRouter(
     {
         'http':get_asgi_application(),
